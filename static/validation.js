e55bc8de294dffe67fa982ca31566e8e77b95cd6
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2022-08-09 15:14:59 +0300

    Regex parse bug

--- static/validation.js
+++ static/validation.js
@@ -45,7 +45,7 @@ function validateY(e) {
 
     let value = parseFloat(yInput.value);
 
-    if (!floatRegex.test(value) || Number.isNaN(value) || value < -5 || value > 5) {
+    if (!floatRegex.test(yInput.value) || Number.isNaN(value) || value < -5 || value > 5) {
         showWarning(yInputWarning, "Invalid Y value. It should be a float from -5 to 5 (Inclusive)");  
         submitButtonEnabler.yValid = false;
     }
